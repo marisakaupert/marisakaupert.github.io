21b1d833864dd14054f26cd38906ccd41fb72522
Marisa Lozano <marisalozano1995@gmail.com>
2017-01-07 21:34:54 -0500

    got jQuery to work

--- js/global.js
+++ js/global.js
@@ -1,16 +1,11 @@
 $(function() {
-	$('ul.nav a').bind('click',function(event){
+	$('.navigationLinks').bind('click',function(event){
 		var $anchor = $(this);
 
 		$('html, body').stop().animate({
 			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1500,'easeInOutExpo');
-		/*
-		if you don't want to use the easing effects:
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1000);
-		*/
+		}, 1000,'easeInOutExpo');
+        
 		event.preventDefault();
 	});
 });
